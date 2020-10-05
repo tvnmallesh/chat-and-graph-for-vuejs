e67c9cf731c329ef4d1dc70dc0425acefc3f40f3
TV Naga Mallesh <29097@ITCINFOTECH.COM>
2020-10-05 18:24:55 +0530

    vuejas for chat control

---
+++ Navbar.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <mdb-navbar position="top" dark color="primary" scrolling>
+      <mdb-navbar-brand href="#"><img alt="MDB logo" src="../assets/logo.png"></mdb-navbar-brand>
+      <mdb-navbar-toggler>
+        <mdb-navbar-nav>
+          <mdb-nav-item href="#" waves-fixed>Home</mdb-nav-item>
+          <mdb-nav-item href="#" waves-fixed>Features</mdb-nav-item>
+          <mdb-nav-item href="#" waves-fixed>Pricing</mdb-nav-item>
+          <mdb-dropdown tag="li" class="nav-item">
+            <mdb-dropdown-toggle
+              tag="a"
+              navLink
+              color="primary"
+              slot="toggle"
+              waves-fixed
+              >Dropdown</mdb-dropdown-toggle
+            >
+            <mdb-dropdown-menu>
+              <mdb-dropdown-item>Action</mdb-dropdown-item>
+              <mdb-dropdown-item>Another action</mdb-dropdown-item>
+              <mdb-dropdown-item>Something else here</mdb-dropdown-item>
+            </mdb-dropdown-menu>
+          </mdb-dropdown>
+        </mdb-navbar-nav>
+        <form>
+          <mdb-input
+            type="text"
+            class="text-white"
+            placeholder="Search"
+            aria-label="Search"
+            label
+            navInput
+            waves
+            waves-fixed
+          />
+        </form>
+      </mdb-navbar-toggler>
+    </mdb-navbar>
+    <div style="height: 110vh">
+      <div class="view intro-2">
+        <div class="full-bg-img">
+          <div class="mask rgba-white-strong">
+            <div class="container flex-center">
+              <div class="dark-grey-text">
+                <h2 class="font-weight-bold">
+                  Fixed navbar
+                </h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import {
+    mdbNavbar,
+    mdbNavItem,
+    mdbNavbarNav,
+    mdbNavbarToggler,
+    mdbDropdown,
+    mdbDropdownItem,
+    mdbDropdownMenu,
+    mdbDropdownToggle,
+    mdbInput,
+    mdbNavbarBrand
+  } from "mdbvue";
+  export default {
+    name: "NavigationPage",
+    components: {
+      mdbNavbar,
+      mdbNavItem,
+      mdbNavbarNav,
+      mdbNavbarToggler,
+      mdbDropdown,
+      mdbDropdownItem,
+      mdbDropdownMenu,
+      mdbDropdownToggle,
+      mdbInput,
+      mdbNavbarBrand
+    },
+  };
+  </script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,80 @@
+<template>
+  <mdb-container>
+    <mdb-pie-chart
+      datalabels
+      :data="pieChartData"
+      :options="pieChartOptions"
+      :width="600"
+      :height="300"
+    />
+  </mdb-container>
+</template>
+<script>
+  import { mdbPieChart, mdbContainer } from "mdbvue";
+  export default {
+    name: "ChartPage",
+    components: {
+      mdbPieChart,
+      mdbContainer
+    },
+    data() {
+      return {
+        pieChartData: {
+          labels: ["Red", "Green", "Yellow", "Grey", "Dark Grey"],
+          datasets: [
+            {
+              data: [300, 50, 100, 40, 120],
+              backgroundColor: [
+                "#F7464A",
+                "#46BFBD",
+                "#FDB45C",
+                "#949FB1",
+                "#4D5360"
+              ],
+              hoverBackgroundColor: [
+                "#FF5A5E",
+                "#5AD3D1",
+                "#FFC870",
+                "#A8B3C5",
+                "#616774"
+              ]
+            }
+          ]
+        },
+        pieChartOptions: {
+          responsive: false,
+          maintainAspectRatio: false,
+          plugins: {
+            datalabels: {
+              color: "white",
+              align: "center",
+              font: {
+                size: 16
+              },
+              formatter: value => {
+                const [dataset] = this.pieChartData.datasets;
+                const setValue = dataset.data.reduce((a, b) => a + b);
+
+                return `${Math.round((value / setValue) * 100)}%`;
+              }
+            }
+          }
+        }
+      };
+    }
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  font-weight: normal;
+  padding-top: 20px;
+  padding-bottom: 30px;
+}
+p {
+  color: #969696;
+  margin-bottom: 0;
+  font-size: 14px;
+}
+</style>
